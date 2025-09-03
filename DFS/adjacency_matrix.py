05141f66af30fafceeca2179d0512615e3633f56
Johnathan Kim <99706992+J0hnathanKim@users.noreply.github.com>
2025-09-03 18:11:21 +0900

    Create adjacency_matrix.py

---
+++ DFS/adjacency_matrix.py
@@ -0,0 +1,10 @@
+INF = 99999999 #represents infinity
+
+# representation of adjacency matrix with using two dimensional array
+graph = [
+    [0, 7, 5]
+    ,[7, 0, INF]
+    ,[5, INF, 0]
+]
+
+print(graph)
