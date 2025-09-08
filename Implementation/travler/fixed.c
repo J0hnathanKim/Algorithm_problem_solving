321143a729c5ae58af80914accd03dea376ce8d5
Johnathan Kim <99706992+J0hnathanKim@users.noreply.github.com>
2025-09-09 08:56:51 +0900

    Create fixed.c

---
+++ Implementation/travler/fixed.c
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int row = 1, col = 1;
+    int count;
+    cin >> count;
+
+    // Ensure count doesn't exceed the number of possible valid moves
+    count = min(count, 4); // The max number of valid moves is 4 on a 5x5 grid
+
+    while (count > 0) {
+        char direction;
+        cin >> direction;
+
+        switch (direction) {
+            case 'R':
+                if (col < 5) {
+                    col++;
+                    count--;
+                }
+                break;
+            case 'D':
+                if (row < 5) {
+                    row++;
+                    count--;
+                }
+                break;
+            case 'U':
+                if (row > 1) {
+                    row--;
+                    count--;
+                }
+                break;
+            case 'L':
+                if (col > 1) {
+                    col--;
+                    count--;
+                }
+                break;
+            default:
+                // Invalid input (not 'R', 'L', 'U', or 'D')
+                break;
+        }
+    }
+
+    cout << row << " " << col << endl;
+}
